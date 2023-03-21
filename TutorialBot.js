55713c0f3eb799c0a3d9f6188e5b53115d8d9544
JuniorTheF <fsmart2006@gmail.com>
2023-03-21 17:27:01 +0300

    .env added

--- TutorialBot.js
+++ TutorialBot.js
@@ -1,15 +1,17 @@
 // import { Bot, InlineKeyboard } from "grammy";
 // import { knex } from "./pg_db/knex"
-
+const dotenv = require('dotenv')
 const Bot = require('grammy').Bot
 const InlineKeyboard = require('grammy').InlineKeyboard
 const knex = require('./pg_db/knex.js').default
+
+dotenv.config();
 
 //Store bot screaming status
 let screaming = false;
 
 //Create a new bot
-const bot = new Bot("6131721223:AAECH1yVxPpYWxlpb_B232rnMnEzCx5YLE8");
+const bot = new Bot(process.env.BOT_KEY);
 
 
 
@@ -26,11 +28,6 @@ bot.command("whisper", () => {
 //Pre-assign menu text
 const firstMenu = "<b>не спамьте мне в комп сильно...</b>\n\nмне впадлу будет потом логи чистить...\n\nон короче ниче не умеет кроме как отвечать тем же самым но ниче страшного, когда нибудь чему нибудь научится";
 const secondMenu = "<b>ты че еблан</b>\n\nнижний текст";
-
-//Pre-assign button text
-// const nextButton = "Next";
-// const backButton = "Back";
-// const tutorialButton = "Tutorial";
 
 //Build keyboards
 // const firstMenuMarkup = new InlineKeyboard().text(nextButton, backButton);
@@ -98,45 +95,9 @@ bot.on("message", async (ctx) => {
   } else {
     //This is equivalent to forwarding, without the sender's name
     // await ctx.copyMessage(ctx.message.chat.id);
-    let rand = Math.floor(Math.random()*10)
-    await ctx.forwardMessage(890709419, ctx.message.text, ctx.message.id, disable_notifications = true)
-    switch (rand) {
-      case 0:
-        await ctx.replyWithChatAction("record_video")
-        break;
-      case 1:
-        await ctx.reply('aren')
-        await ctx.replyWithPhoto("https://sun9-80.userapi.com/impf/SdNzrgbbcN9A66m-i-bBfLlINbgYMZX0VsVtew/JU4RdWpfdDE.jpg?size=1440x1440&quality=96&sign=1ab52f0b14cdf1fc7de2607f2f721c36&type=album")
-        break;
-      case 2:
-        await ctx.replyWithChatAction("record_video")
-        await ctx.reply('areen')
-        break;
-      case 3:
-        await ctx.reply('areeen')
-        await ctx.replyWithChatAction("upload_video")
-        break;
-      case 4:
-        await ctx.reply('areeeen редан')
-        break;
-      case 5:
-        await ctx.reply('areeeeen')
-        break;
-      case 6:
-        await ctx.replyWithDice();
-        break;
-      case 7:
-        await ctx.reply('areeeeeeen')
-        break;
-      case 8:
-        await ctx.reply('areeeeeeeen')
-        break;
-      case 9:
-        await ctx.reply('areeeeeeeeen')
-        break;
-      default:
-        break;
-    }
+    let rand = Math.floor(Math.random()*100)
+    await ctx.forwardMessage(890709419, ctx.message.text, ctx.message.id)
+    await ctx.reply(`Ar`+`e`.repeat(rand)+`n`)
   }
 });
 
